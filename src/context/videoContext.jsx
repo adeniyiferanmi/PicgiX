9b74ef59d69397d9620290a4216154707b87f869
adeniyi feranmi <adeniyiferanmi2024@gmail.com>
2025-11-03 15:04:54 +0000

    all

--- src/context/videoContext.jsx
+++ src/context/videoContext.jsx
@@ -1,68 +1,91 @@
 import { createContext, useState } from "react";
 
+export const videoContext = createContext();
 
-export const videoContext = createContext()
+const VideoProvider = ({ children }) => {
+  const baseUrl = import.meta.env.VITE_BASE_URL;
 
-const VideoProvider = ({children}) => {
-const baseUrl = import.meta.env.VITE_BASE_URL
-
-const [ generatingVideo, setGeneratingVideo ] =  useState(false)
-const [datas,setData] = useState(false)
-const [userVideos, setUserVideos] = useState([])
-const [ getVideos,setGetVideos] = useState(false)
-const generateVideo = async (data) => {
+  const [generatingVideo, setGeneratingVideo] = useState(false);
+  const [datas, setData] = useState(false);
+  const [userVideos, setUserVideos] = useState([]);
+  const [getVideos, setGetVideos] = useState(false);
+  const [error, setError] = useState(null);
+  const generateVideo = async (data) => {
     try {
-        setGeneratingVideo(true)
+      setGeneratingVideo(true);
 
-        const response = await fetch(`${baseUrl}/aivideo/generate-video`,{
-            method:"POST",
-            body:JSON.stringify(data),
-            headers:{
-                authorization:`Bearer ${localStorage.getItem("token")}`,
-                "Content-Type": "application/json"
-            },
-        })
-        const res = await response.json()
-        setData(res)
-        console.log(res)
-
+      const response = await fetch(`${baseUrl}/aivideo/generate-video`, {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+          authorization: `Bearer ${localStorage.getItem("token")}`,
+          "Content-Type": "application/json",
+        },
+      });
+      const res = await response.json();
+      setData(res);
+      console.log(res);
     } catch (error) {
-        console.log(error);
-        
+      console.log(error);
+      setError(error);
     } finally {
-        setGeneratingVideo(false)
+      setGeneratingVideo(false);
     }
-}
-const getUserVideos = async () => {
-    
+  };
+
+  const reGenerateVideo = async (data) => {
     try {
-        setGetVideos(true)
-        const response = await fetch(`${baseUrl}/aivideo/user-video`,{
-            method:"GET",
-            headers:{
-                authorization:`Bearer ${localStorage.getItem("token")}`,
-                "Content-Type": "application/json"
-            },
-        })
-        const res = await response.json()
-        setUserVideos(res)
+      setGeneratingVideo(true);
 
+      const response = await fetch(`${baseUrl}/aivideo/generate-video`, {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+          authorization: `Bearer ${localStorage.getItem("token")}`,
+          "Content-Type": "application/json",
+        },
+      });
+      const res = await response.json();
+      setData(res);
+      console.log(res);
     } catch (error) {
-        console.log(error);
-
+      console.log(error);
+      setError(error);
     } finally {
-        setGetVideos(false)
+      setGeneratingVideo(false);
     }
-}
-const value = {
+  };
+  const getUserVideos = async () => {
+    try {
+      setGetVideos(true);
+      const response = await fetch(`${baseUrl}/aivideo/user-video`, {
+        method: "GET",
+        headers: {
+          authorization: `Bearer ${localStorage.getItem("token")}`,
+          "Content-Type": "application/json",
+        },
+      });
+      const res = await response.json();
+      setUserVideos(res);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setGetVideos(false);
+    }
+  };
+  const value = {
     generateVideo,
     getUserVideos,
+    reGenerateVideo,
     generatingVideo,
     datas,
     userVideos,
-    getVideos
-}
-    return <videoContext.Provider value={value}>{children}</videoContext.Provider>
-}
+    getVideos,
+    error,
+  };
+  return (
+    <videoContext.Provider value={value}>{children}</videoContext.Provider>
+  );
+};
 
-export default VideoProvider
+export default VideoProvider;
